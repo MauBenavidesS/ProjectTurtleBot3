933e7abd6178d99cabf72f0f099374f640b6838c
root <root@Toyota>
2024-06-05 13:32:01 +0900

    [v2] Obstacle Avoidance V2

--- src/turtlebot3_avoidance/scripts/turtlebot3_avoidance.py
+++ src/turtlebot3_avoidance/scripts/turtlebot3_avoidance.py
@@ -5,17 +5,12 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
 def callbackLaserScan(data):
-    # regions = {
-    #     'right':  min(min(data.ranges[0:143]), 10),
-    #     'front':  min(min(data.ranges[144:287]), 10),
-    #     'left':   min(min(data.ranges[288:431]), 10),
-    # }
     regions = {
-        'left':   data.ranges[320],
-        'front-left':   data.ranges[340],
+        'left':   data.ranges[330],
+        'front-left':   data.ranges[350],
         'front':  data.ranges[0],
-        'front-right':  data.ranges[20],
-        'right':  data.ranges[40],
+        'front-right':  data.ranges[10],
+        'right':  data.ranges[30],
     }
 
     take_action(regions)
@@ -30,34 +25,28 @@ def take_action(regions):
     print("front: {0:.2f}, left: {1:.2f}, right: {2:.2f}".format(regions['front'], regions['left'], regions['right']))
 
     # Adjusting thresholds for more responsive obstacle avoidance
-    front_threshold = 0.7
-    side_threshold = 0.4
+    side_threshold = 0.6
 
-    if regions['front'] < front_threshold:
-        state_description = 'case 1 - turn'
-        linear_x = 0
-        angular_z = 1
-    elif regions['front-left'] < side_threshold:
-        state_description = 'case 2 - right'
+    if regions['left'] < side_threshold:
+        state_description = 'case 4 - Turn Right'
         linear_x = 0
         angular_z = 0.75
-    elif regions['front-right'] < side_threshold:
-        state_description = 'case 3 - right'
+    elif regions['right'] < side_threshold:
+        state_description = 'case 5 - Turn Left'
         linear_x = 0
         angular_z = -0.75
-    elif regions['left'] < side_threshold:
-        state_description = 'case 4 - right'
-        linear_x = 0
-        angular_z = 0.5
-    elif regions['right'] < side_threshold:
-        state_description = 'case 5 - right'
-        linear_x = 0
-        angular_z = -0.5
+    elif regions['front-left'] < side_threshold:
+        state_description = 'case 2 - Sharp Right'
+        linear_x = 0.2
+        angular_z = 0.75
+    elif regions['front-right'] < side_threshold:
+        state_description = 'case 3 - Sharp Left'
+        linear_x = 0.2
+        angular_z = -0.75
     else:
         state_description = 'case 0 - advance'
         linear_x = 0.5
         angular_z = 0
-        # rospy.loginfo(regions)
 
     rospy.loginfo(state_description)
     msg.linear.x = linear_x
